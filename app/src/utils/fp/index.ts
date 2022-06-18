67a9e5d0e172dc3322f58b744ff82325b3b38a2e
Kamil Natonek <kamil.natonek@withintent.com>
2022-06-18 16:33:54 +0200

    clean up

--- app/src/utils/fp/index.ts
+++ app/src/utils/fp/index.ts
@@ -2,6 +2,3 @@ import * as P from './match'
 
 export { P }
 export { toBoolean } from './toBoolean'
-export { ascendBy } from './ascendBy'
-export { descendBy } from './descendBy'
-export { failwith, Failure } from './failwith'
